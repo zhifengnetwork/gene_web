67cf2b3f7616b3a2394fdf196d22f83d38b967e4
webhzp <1225613448@qq.com>
2019-04-16 15:53:59 +0800

    导入数据

--- js/public/footer.js
+++ js/public/footer.js
@@ -1,23 +1,21 @@
 var footer = (`
-    <div class="footer">
-        <div class="inner-wrap">
-            <div class="left fl">
-                <div class="bottomNav">
-                    <ul>
-                        <li><a href="#">基因检测</a></li>
-                        <li><a href="#">什么是基因</a></li>
-                        <li><a href="#">研究所</a></li>
-                        <li><a href="#">联系我们</a></li>
-                    </ul>
-                </div>
-                <div class="copyright">
-                    <p>iuehgkdll.cn <span>粤C</span></p>
-                </div>
+    <div class="inner-wrap">
+        <div class="left fl">
+            <div class="bottomNav">
+                <ul>
+                    <li><a href="#">基因检测</a></li>
+                    <li><a href="#">什么是基因</a></li>
+                    <li><a href="#">研究所</a></li>
+                    <li><a href="#">联系我们</a></li>
+                </ul>
             </div>
-            <div class="code fl">
-                <img src="../../images/public/code.png" />
-                <p>扫码关注我们</p>
+            <div class="copyright">
+                <p>iuehgkdll.cn <span>粤C</span></p>
             </div>
+        </div>
+        <div class="code fl">
+            <img src="../../images/public/code.png" />
+            <p>扫码关注我们</p>
         </div>
     </div>
 `);
